cabb3bc749820916c17d3c853b77f54e3c2f53fe
laibaekram <111961920+laibaekram@users.noreply.github.com>
2023-12-10 15:14:33 -0600

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -6,60 +6,34 @@ import tkinter as tk
 
 app = Flask(__name__)
 
+def create_map(location, tiles='OpenStreetMap', zoom_start=13):
+    m = folium.Map(location=location, tiles=tiles, zoom_start=zoom_start)
+    folium.Marker(location, popup="<b>User Selected Location</b>").add_to(m)
+    return m._repr_html_()
+
 @app.route("/")
-# This is the base map
 def base():
-    return generate_map([45.52336, -122.6750])
+    return create_map([45.52336, -122.6750])
 
 @app.route("/open-street-map")
-# This map uses Stamen Toner
 def open_street_map():
-    m = folium.Map(
-        location=[45.52336, -122.6750],
-        tiles='Stamen Toner',
-        zoom_start=13
-    )
-    folium.Marker(
-        location=[45.52336, -122.6750],
-        popup="<b>Marker here</b>",
-        tooltip="Click Here!"
-    ).add_to(m)
-    return m._repr_html_()
+    return create_map([45.52336, -122.6750], tiles='Stamen Toner')
 
 @app.route("/map-marker")
 def map_marker():
-    m = folium.Map(
-        location=[45.52336, -122.6750],
-        tiles='Stamen Terrain',
-        zoom_start=12
-    )
-    folium.Marker(
-        location=[45.52336, -122.6750],
-        popup="<b>Marker here</b>",
-        tooltip="Click Here!"
-    ).add_to(m)
-
-    folium.Marker(
-        location=[45.53236, -122.8750],
-        popup="<b>Marker 2 here</b>",
-        tooltip="Click Here!",
-        icon=folium.Icon(color='green')
-    ).add_to(m)
-
-    folium.Marker(
-        location=[45.54236, -122.8750],
-        popup="<b>Marker 3 here</b>",
-        tooltip="Click Here!",
-        icon=folium.Icon(color='red')
-    ).add_to(m)
+    m = folium.Map(location=[45.52336, -122.6750], tiles='Stamen Terrain', zoom_start=12)
+    folium.Marker(location=[45.52336, -122.6750], popup="<b>Marker here</b>", tooltip="Click Here!").add_to(m)
+    folium.Marker(location=[45.53236, -122.8750], popup="<b>Marker 2 here</b>", tooltip="Click Here!", icon=folium.Icon(color='green')).add_to(m)
+    folium.Marker(location=[45.54236, -122.8750], popup="<b>Marker 3 here</b>", tooltip="Click Here!", icon=folium.Icon(color='red')).add_to(m)
     return m._repr_html_()
 
-def generate_map(location):
-    m = folium.Map(location=location)
-    html = m.get_root().render()
-    with open("map.html", "w") as f:
-        f.write(html)
-    return f"Map generated at {location}"
+@app.route("/custom-map/<coords>")
+def custom_map(coords):
+    try:
+        lat, lon = map(float, coords.split(','))
+        return create_map([lat, lon])
+    except ValueError:
+        return "Invalid coordinates", 400
 
 def start_flask():
     app.run(debug=False, use_reloader=False)
@@ -70,6 +44,22 @@ def open_browser():
 def create_tkinter_window():
     root = tk.Tk()
     root.title("Flask and Tkinter Integration")
+
+    tk.Label(root, text="Latitude:").pack()
+    lat_entry = tk.Entry(root)
+    lat_entry.pack()
+
+    tk.Label(root, text="Longitude:").pack()
+    lon_entry = tk.Entry(root)
+    lon_entry.pack()
+
+    def open_custom_map():
+        lat = lat_entry.get()
+        lon = lon_entry.get()
+        webbrowser.open(f'http://127.0.0.1:5000/custom-map/{lat},{lon}')
+
+    custom_map_button = tk.Button(root, text="Custom Map", command=open_custom_map)
+    custom_map_button.pack()
 
     button1 = tk.Button(root, text="Base Map", command=lambda: webbrowser.open('http://127.0.0.1:5000/'))
     button1.pack()
@@ -83,10 +73,6 @@ def create_tkinter_window():
     root.mainloop()
 
 if __name__ == "__main__":
-    # Start Flask in a separate thread
     flask_thread = threading.Thread(target=start_flask)
     flask_thread.start()
-
-    # Generate Tkinter window
     create_tkinter_window()
-
